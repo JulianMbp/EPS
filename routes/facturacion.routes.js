df31a122b983559ae3178db7aa7bacad6699dcc0
JulianMbp <julian_mtb993@outlook.es>
2024-09-08 16:57:46 -0500

    se agrego metodo get en facturacion

---
+++ routes/facturacion.routes.js
@@ -0,0 +1,16 @@
+/**
+ * @author daferarte
+ * @version 1.0.0
+ * 
+ * Rutas de usuario
+ * Este archivo define las rutas de usuarios
+ */
+const {Router} = require('express');
+
+const router= Router();
+
+const {ShowFacturacion} = require('../controllers/facturacion.controllers');
+
+router.get('/', ShowFacturacion);
+
+module.exports = router;
