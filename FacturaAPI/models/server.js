551c44fc57599c00e9ae09a762d7254c8e72841f
JulianMbp <julian_mtb993@outlook.es>
2024-11-17 18:30:15 -0500

    j

---
+++ FacturaAPI/models/server.js
@@ -0,0 +1,45 @@
+/**
+ * @author EPS
+ * @version 1.0.0
+ * 
+ * Servidor de express para la gestión de Citas Médicas
+ * Esta clase inicia el servidor y define las rutas y middlewares
+ */
+
+const express = require('express');
+
+/**
+ * @class Server
+ * clase servidor que inicia el servicio de express
+ */
+class Server{
+
+    constructor(){
+        this.app = express();
+        this.port = 3001;
+        this.paths = {
+            facturacion: '/api/factuacion'
+        };
+
+        this.middlewares();
+        this.routes();
+    }
+
+
+    middlewares(){
+
+        this.app.use(express.json());
+    }
+    routes(){
+        this.app.use(this.paths.citas, require('../routes/facturacion.routes'));
+    }
+
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log(`Servidor funcionando en el puerto: ${this.port}`); 
+        });
+    }
+}
+
+module.exports = Server;
